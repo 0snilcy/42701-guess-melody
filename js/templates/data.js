dfa604e975cb04e2494fc4c113d9efbec6bd60d5
Oleg Zubov <zuoa@technopark.ru>
2017-06-07 21:06:08 +0300

    travis

--- js/templates/data.js
+++ js/templates/data.js
@@ -12,10 +12,10 @@ export const levelArtist = Object.freeze({
   answers: [{
     title: `Пелагея`,
     pick: `https://images.unsplash.com/photo-1495458200315-8a1dce4bbe77?dpr=1&auto=compress,format&fit=max&w=576&q=80&cs=tinysrgb&crop=`
-  },{
+  }, {
     title: `Краснознаменная дивизия имени моей бабушки`,
     pick: `https://images.unsplash.com/photo-1491933367339-d869a4dcc137?dpr=1&auto=compress,format&fit=max&w=576&q=80&cs=tinysrgb&crop=`
-  },{
+  }, {
     title: `Тибетское горловое пение`,
     pick: `https://images.unsplash.com/photo-1481762554390-ff5562748bdf?dpr=1&auto=compress,format&fit=max&w=376&q=80&cs=tinysrgb&crop=`
   }],
