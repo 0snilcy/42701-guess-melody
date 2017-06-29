14fcc7d416b2a3255d904b61a7c2b4e28232b80f
Oleg Zubov <zuoa@technopark.ru>
2017-06-29 12:39:31 +0300

    Разделил экран результатов на 2 вьюхи

--- js/templates/result/controller.js
+++ js/templates/result/controller.js
@@ -2,14 +2,38 @@
  * Created by wakedafuckup on 25.06.17.
  */
 
-import {ResultView} from './view';
-import {data as model} from './model';
+import {DefeatView} from './defeatView';
+import {VictoryView} from './victoryView';
+import data from './model';
 import showScreen from '../../tools/showScreen';
 import Application from '../../main';
+import BaseModel from '../../tools/baseModel';
+import getUserRating from './getUserRating';
 
 export class Result {
-  init(data) {
-    this.view = new ResultView(data, model);
+  init(stats) {
+    console.log(stats);
+
+    if (stats.lives) {
+      this.model = new class extends BaseModel {
+        get urlRead() {
+          return `https://intensive-ecmascript-server-btfgudlkpi.now.sh/guess-melody/stats/42701`;
+        }
+        get urlWrite() {
+          return `https://intensive-ecmascript-server-btfgudlkpi.now.sh/guess-melody/stats/42701`;
+        }
+      }();
+
+      this.model.send(data).then((data) => console.log(data));
+
+      this.view = new VictoryView(data.victory, stats);
+    } else {
+      this.view = new DefeatView(data.defeat);
+    }
+
+    // this.model.send()
+    //   .then(() => console.log(`Dannie otpravleny`));
+
     this.view.btnEvent = () => Application.showWelcome();
     showScreen(this.view.getMarkup);
   }
--- js/templates/result/model.js
+++ js/templates/result/model.js
@@ -2,12 +2,13 @@
  * Created by wakedafuckup on 12.06.17.
  */
 
-export const data = Object.freeze({
+export default Object.freeze({
   victory: {
     title: `Вы настоящий меломан!`
   },
   defeat: {
-    title: `Вы проиграли`
+    title: `Вы проиграли`,
+    subtitle: `Ничего, вам повезет в следующий раз`
   }
 });
 
